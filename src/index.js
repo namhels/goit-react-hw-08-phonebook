d30a31efb90e2c17fb7be41bed45aaf34e53f888
namhels <8.lsv.888@gmail.com>
2023-02-23 20:21:01 +0300

    add PersistGate, auth

--- src/index.js
+++ src/index.js
@@ -2,15 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from 'App';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
+import { store, persistor } from './redux/store';
 import { ThemeProvider } from 'styled-components';
-import { store } from './redux/store';
 import { theme } from 'utils';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter basename="/goit-react-hw-08-phonebook">
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
+    </PersistGate>
   </Provider>
 );
---
+++ src/redux/auth/selectors.js
@@ -0,0 +1,23 @@
+// import { createSelector } from '@reduxjs/toolkit';
+
+// export const selectContacts = state => state.contacts.items;
+// export const selectIsLoading = state => state.contacts.isLoading;
+// export const selectError = state => state.contacts.error;
+// export const selectFilter = state => state.filter.filter;
+
+// // ===============<<< Memo Selector >>>=================
+// export const selectVisibleContacts = createSelector(
+//   [selectContacts, selectFilter],
+//   (contacts, filter) => {
+//     const normalizedFilter = filter.toLowerCase().trim();
+//     return contacts.filter(contact =>
+//       contact.name.toLowerCase().trim().includes(normalizedFilter)
+//     );
+//   }
+// );
+
+export const selectIsLoggedIn = state => state.auth.isLoggedIn;
+
+export const selectUser = state => state.auth.user;
+
+export const selectIsRefreshing = state => state.auth.isRefreshing;
